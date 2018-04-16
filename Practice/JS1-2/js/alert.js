a23fe9b59f18890d977f05e9f7dc4266079f3cdb
Eugenia Zolotarenko <eu.zolotarenko@gmail.com>
2018-04-16 16:31:24 +0300

    JS HW 1-2. Function Pow

---
+++ Practice/JS1-2/js/alert.js
@@ -0,0 +1,37 @@
+function pow (x,n) {
+
+    var result = x;
+
+    switch (true) {
+        case (n === 0):
+            result = 1;
+            break;
+            
+        case (n > 0):
+            for ( var i=1; i < n; i++) {
+            result = result*x;
+            }
+            break;
+
+        case (n < 0):
+            for ( var i = 1; i < -n; i++) {
+                result = result*x;
+            }
+            result = '1/' + result;
+            break;
+
+        default:
+            result = 'something is wrong';
+    }
+
+    return result;
+}
+
+var x = +prompt('Enter Number', ''),
+    n = +prompt('Enter stepen', '');
+
+//var y = pow(x,n);
+
+alert (pow(x,n));
+
+
