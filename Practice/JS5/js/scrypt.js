729152d0d3cabaaf44cae21b497e5986d3549a57
Женечка <zenecka@Alekss-MacBook-Pro.local>
2018-05-28 16:16:24 +0300

    JS5-HW. Timer works

--- Practice/JS5/js/scrypt.js
+++ Practice/JS5/js/scrypt.js
@@ -1,13 +1,15 @@
+var time = document.getElementById("siteTimer");
+var mlsec = document.getElementById("timer__mlsec");
 
 
 function myTimer() {
-    var siteTimer = new Date(); //создаём объект Date()
+     siteTimer = new Date(); //создаём объект Date()
     siteTimer.setHours(0, 0, 0, 0);
 
-    var hour = siteTimer.getHours(); //получаем часы
-    var minute = siteTimer.getMinutes(); //получаем минуты
-    var second = siteTimer.getSeconds(); //получаем секунды
-    var millisecond = siteTimer.getMilliseconds();
+     hour = siteTimer.getHours(); //получаем часы
+     minute = siteTimer.getMinutes(); //получаем минуты
+     second = siteTimer.getSeconds(); //получаем секунды
+     millisecond = siteTimer.getMilliseconds();
     //В следующих трех строках проверяем, если число (часы, минуты, секунды) меньше 10,
     // то выводим 0 перед числом (для красоты)
     if (hour < 10) hour = "0" + hour;
@@ -17,45 +19,57 @@ function myTimer() {
     if (millisecond < 100 & millisecond > 10) millisecond = "0" + millisecond;
 
     //Находим на странице элемент с id = siteTime и внутрь него записываем время сайта
-    document.getElementById("siteTimer").innerHTML = hour + ":" + minute + ":" + second;
-    document.getElementById("timer__mlsec").innerHTML =  millisecond;
+    time.innerHTML = hour + ":" + minute + ":" + second;
+    mlsec.innerHTML =  millisecond;
 }
 
 function timerStart() {
     millisecond = 0;
-    var siteTimer = new Date(); //создаём объект Date()
-    siteTimer.setHours(0, 0, 0, 0);
 
-    var hour = siteTimer.getHours(); //получаем часы
-    var minute = siteTimer.getMinutes(); //получаем минуты
-    var second = siteTimer.getSeconds(); //получаем секунды
-    var millisecond = siteTimer.getMilliseconds();
-    //В следующих трех строках проверяем, если число (часы, минуты, секунды) меньше 10,
-    // то выводим 0 перед числом (для красоты)
-    if (hour < 10) hour = "0" + hour;
-    if (minute < 10) minute = "0" + minute;
-    if (second < 10) second = "0" + second;
-    if (millisecond < 10) millisecond = "00" + millisecond;
-    if (millisecond < 100 & millisecond > 10) millisecond = "0" + millisecond;
-
-    var idInt = setInterval(function() {
+    idStartInt = setInterval(function() {
 
         //Находим на странице элемент с id = siteTime и внутрь него записываем время сайта
-        document.getElementById("siteTimer").innerHTML = hour + ":" + minute + ":" + second;
-        document.getElementById("timer__mlsec").innerHTML =  millisecond;
-        if (millisecond > 999) { second++;
+
+         time.innerHTML = hour + ":" + minute + ":" + second;
+         mlsec.innerHTML =  millisecond;
+
+        if (millisecond > 99) { second++;
                                  millisecond = 0;}
 
 
+        if (second > 59) { minute++;
+            second = 0;}
 
+        if (minute > 59) { hour++;
+            minute = 0;}
 
         millisecond++;
-
-
-
-    }, 1);
+    }, 10);
 }
 
+
+function timerContinue() {
+
+    idContinueInt = setInterval(function() {
+
+        //Находим на странице элемент с id = siteTime и внутрь него записываем время сайта
+
+        time.innerHTML = hour + ":" + minute + ":" + second;
+        mlsec.innerHTML =  millisecond;
+
+        if (millisecond > 99) { second++;
+            millisecond = 0;}
+
+
+        if (second > 59) { minute++;
+            second = 0;}
+
+        if (minute > 59) { hour++;
+            minute = 0;}
+
+        millisecond++;
+    }, 10);
+}
 
 
 var start = document.getElementById("start");
@@ -64,6 +78,7 @@ var contin = document.getElementById("continue");
 var clear = document.getElementById("clear");
 
 function heandlerStart () {
+
     start.style.display = "none";
     pause.style.display = "block";
     timerStart();
@@ -73,18 +88,22 @@ function heandlerStart () {
 function heandlerPause () {
     contin.style.display = "block";
     pause.style.display = "none";
+    clearInterval(idStartInt);
+    clearInterval(idContinueInt);
 }
 
 function heandlerContinue () {
     contin.style.display = "none";
     pause.style.display = "block";
+    timerContinue();
 }
 
 function heandlerClear () {
+    clearInterval(idStartInt);
+    clearInterval(idContinueInt);
     contin.style.display = "none";
     pause.style.display = "none";
     start.style.display = "block";
-
     myTimer();
 }
 
