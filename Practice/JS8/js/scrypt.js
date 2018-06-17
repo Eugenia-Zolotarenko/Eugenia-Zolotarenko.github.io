c259dff88ac76c38158fd3639f919b3ef91806d4
Eugenia Zolotarenko <eu.zolotarenko@gmail.com>
2018-06-17 22:38:32 +0300

    JS HW 8. Show-up hints & Button Help. Refactor

--- Practice/JS8/js/scrypt.js
+++ Practice/JS8/js/scrypt.js
@@ -1,8 +1,9 @@
-
 $(document).ready(function () {
     $("div").append("<em></em>");
+
     var titles = $('input[title]');
     var ems = $('em');
+
     for ( var i = 0; i < titles.length; i++ ) {
         $(ems[i]).text( $(titles[i]).attr("title"));
     }
@@ -11,34 +12,11 @@ $(document).ready(function () {
         function(){
             event.preventDefault();
             $(this).next("em").show();
-
     },  function(){
             $(this).next("em").hide();
-
     });
 
     $('input[type="button"]').on( "click", function() {
         $('em').show();
-        //setTimeout(function() { $("em").hide('slow'); }, 2000);
-        });
-});
-
-
-
-
-/*$( function() {
-    var tooltips = $( "[title]" ).tooltip({
-        position: {
-            my: "left top",
-            at: "right+5 top-5",
-            collision: "none"
-        }
     });
-    $( "<button>" )
-        .text( "Show help" )
-        .button()
-        .on( "click", function() {
-            tooltips.tooltip( "open" );
-        })
-        .insertAfter( "form" );
-} );*/
+});
