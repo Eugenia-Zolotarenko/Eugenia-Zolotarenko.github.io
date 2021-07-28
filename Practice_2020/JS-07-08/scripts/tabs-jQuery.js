fda6495570c1ba9393db2f1b28a3ebde4b330ce3
Eugenia Zolotarenko <eu.zolotarenko@gmail.com>
2021-07-28 13:29:18 +0300

    JS-07-08. Tabs. Edded Handler for Arrows and Ctrl+Arrows

--- Practice_2020/JS-07-08/scripts/tabs-jQuery.js
+++ Practice_2020/JS-07-08/scripts/tabs-jQuery.js
@@ -2,15 +2,18 @@
 
 $(function(){
     let $tabs = $('#tabs');
+    let isKeyUpOn;
 
     showFirstTab($tabs);
 
-    $tabs.on('click', '.tab__link', null,  openTab);
+    $tabs.on('click', '.tab__link', null,  openTab)
+         .on('keydown', '.tab__link' , null, moveFocus)
+         .on('keyup', '.tab__link',null, openTabWithKey);
 
 
     function showFirstTab (tabList) {
         let $firstTab = tabList.children(':first-child');
-        let $firstTabContentId = $firstTab.attr('href'); /*;tabContent Id = tab-link 'href' attribute*/
+        let $firstTabContentId = $firstTab.attr('href'); /*tabContent Id = tab-link 'href' attribute*/
         let $firstTabContent =  $($firstTabContentId);
 
         $firstTab.addClass("is-active");  /*add an "is-active" class to the link that opened the tab*/
@@ -32,6 +35,8 @@ $(function(){
         $(this).addClass("is-active");
     }
 
+    /*check if Control keys pressed to handle Cltr+ArrowLeft|ArrowRight keydown*/
+
     let ctrlIsPressed;
 
     $(window).on('keydown', null, null, function (event) {
@@ -42,34 +47,38 @@ $(function(){
         if( event.code === 'ControlLeft' || event.code === 'ControlRight' ) { ctrlIsPressed = false; }
     });
 
-    $tabs.on('keydown', '.tab__link' , null, openTabWithKeydown);
 
-    function openTabWithKeydown(event) {
+    function moveFocus (event) {
+
+        //if (event.code === 'Enter' || event.code === 'NumpadEnter')  event.preventDefault();
+
+
         let $tabindex = $(this).attr('tabindex');
         let $nextTab;
-        console.log(event.code);
+        let codes = ['ArrowRight', 'ArrowUp', 'ArrowLeft', 'ArrowDown', 'Tab', 'Enter', 'NumpadEnter' ];
 
+        if (!codes.includes(event.code) ) return;
 
-        // Get all elements with class="tab__content" and hide them
-        $(".tab__content").hide();
-
-        // Get all elements with class="tab__link" and remove the class "is-active"
-        $(".tab__link").removeClass('is-active');
-
-
-        /* computing the nex tab to be focused and shown*/
+        /* computing the nex tab to be focused*/
         switch (true) {
-            case (event.code === 'ArrowRight' || event.code === 'ArrowUp' /*|| event.code === 'Tab'*/):
+            case (event.code === 'ArrowRight' || event.code === 'ArrowUp' || event.code === 'Tab'):
                 $tabindex++;
+                if ( isKeyUpOn === false)  {
+                    $tabs.on('keyup', '.tab__link', null, openTabWithKey);
+                    isKeyUpOn = true;
+                }
                 break;
 
             case (event.code === 'ArrowLeft' || event.code === 'ArrowDown'):
                 $tabindex--;
+                if ( isKeyUpOn === false)  {
+                    $tabs.on('keyup', '.tab__link', null, openTabWithKey);
+                    isKeyUpOn = true;
+                }
                 break;
 
-           /* case ( ctrlIsPressed  && event.code === 'ArrowLeft' ):
-                $tabindex--;
-                console.log(ctrlIsPressed);
+           /* case (event.code === 'Enter' || event.code === 'NumpadEnter'):
+                $(this).focus();
                 break;*/
         }
 
@@ -79,15 +88,46 @@ $(function(){
         if ($tabindex > $lastTabIndex)  { $tabindex = $firstTabIndex; }
         if ($tabindex < $firstTabIndex) { $tabindex = $lastTabIndex;  }
 
+
         $nextTab = $(`[tabindex= ${$tabindex}]`);
+        $nextTab.focus();
 
-        $nextTab.focus()
-                .addClass("is-active");
 
-        let $tabContentId = $nextTab.attr('href');
+        /*CTRL + DOWN/LEFT: Move focus to the previous tab.
+          If on first tab, moves focus to last tab. */
+        if ( ctrlIsPressed  && (event.code === 'ArrowLeft' || event.code === 'ArrowDown') )  {
+            $tabindex--;
+            $nextTab.focus();
+            $tabs.off('keyup', '.tab__link', openTabWithKey); /*The focused tab must be manually activated*/
+            isKeyUpOn = false;
 
-        $($tabContentId).fadeIn('slow');
+        } else {
+            /*CTRL + UP/RIGHT: Move focus to the previous tab.
+                     If on first tab, moves focus to last tab. The focused tab must be manually activated.*/
+            if (ctrlIsPressed && (event.code === 'ArrowRight' || event.code === 'ArrowUp')) {
+                $tabindex++;
+                $nextTab.focus();
+                $tabs.off('keyup', '.tab__link', openTabWithKey); /*The focused tab must be manually activated*/
+                isKeyUpOn = false;
+            }
+        }
+
     }
-});
+
+    function openTabWithKey(event)  {
+
+            let $tabContentId = $(this).attr('href');
+
+            // Get all elements with class="tab__content" and hide them
+            $(".tab__content").hide();
+
+            // Get all elements with class="tab__link" and remove the class "is-active"
+            $(".tab__link").removeClass('is-active');
+
+            $(this).addClass("is-active"); /*add an "is-active" class to the link that opened the tab*/
+
+            $($tabContentId).fadeIn('slow'); /*Show the focused tab*/
+    }
+} );
 
 
