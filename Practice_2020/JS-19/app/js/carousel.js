1dce76815f86ea5b12d781fbe53a9fd375796d78
Eugenia Zolotarenko <eu.zolotarenko@gmail.com>
2022-02-17 15:41:00 +0200

    JS-19. Added Carousel

---
+++ Practice_2020/JS-19/app/js/carousel.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $('.carousel').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows:true,
+  dots:true,
+
+
+});
+
+});
--- Practice_2020/JS-19/gulpfile.js
+++ Practice_2020/JS-19/gulpfile.js
@@ -45,7 +45,7 @@ function browsersync() {
 }
 
 function scripts () {
-    return src( ['app/js/*.js', '!app/js/*.min.js'] )
+    return src( [ 'app/js/plugins/*.js', 'app/js/*.js', '!app/js/*.min.js'] )
         .pipe(rigger())
         .pipe(concat('app.min.js'))
         .pipe(uglify())
