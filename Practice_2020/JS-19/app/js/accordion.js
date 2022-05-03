3618f2842ca26cebe2292f16b57c5a87d4323cda
Eugenia Zolotarenko <eu.zolotarenko@gmail.com>
2022-05-03 11:38:40 +0300

    added Banners block

--- Practice_2020/JS-19/app/js/accordion.js
+++ Practice_2020/JS-19/app/js/accordion.js
@@ -2,8 +2,7 @@ $(function (){
     $( "#accordion" ).accordion({
         collapsible: true,
         heightStyle: "content",
-        icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
-
+        icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" },
     }
 
     );
